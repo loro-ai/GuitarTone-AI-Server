8ef24187fdbbc42b65eb13c6e16fd67e4c56f14f
loro.ai <loro.ia.col@gmail.com>
2026-03-19 11:00:02 -0500

    fix errores in index.tss

--- src/_core/index.ts
+++ src/_core/index.ts
@@ -12,12 +12,9 @@ async function startServer() {
   const app = express();
   const server = createServer(app);
 
-app.use(cors({
-  origin: true,  // Temporal
-  credentials: true
-}));
 
-/*app.use(cors({
+
+app.use(cors({
   origin: function(origin, callback) {
     const permitidos = [
       'http://localhost:5173',  // dev web
@@ -34,7 +31,7 @@ app.use(cors({
     }
   },
   credentials: true,
-}));*/
+}));
 
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
