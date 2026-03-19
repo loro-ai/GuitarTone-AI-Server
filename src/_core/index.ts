aa8ad34bb31d2b0831bef25f9602271cd81a8b1f
loro.ai <loro.ia.col@gmail.com>
2026-03-19 16:06:47 -0500

    fix not crypto defined

--- src/_core/index.ts
+++ src/_core/index.ts
@@ -1,4 +1,9 @@
 import "dotenv/config";
+import crypto from "crypto"; // 👈 AÑADE ESTO
+if (!globalThis.crypto) {
+  // @ts-ignore
+  globalThis.crypto = crypto.webcrypto;
+}
 import express from "express";
 import { createServer } from "http";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
